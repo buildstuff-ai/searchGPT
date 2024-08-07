e5ad0fe66ebf01505603bb1bf08c1ce9411ff149
HRUSHIKESH DOKALA <96101829+Hk669@users.noreply.github.com>
2024-08-07 22:37:39 +0530

    1. added the chromadb support for the vectorstore, 2. added init files for module identification and requirements (#1)

--- src/services/chromaservice.py
+++ src/services/chromaservice.py
@@ -1,3 +1,24 @@
-"""
-Intial Setup
-"""
+from sqlite3 import DatabaseError
+import chromadb
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_chromadb_collection():
+    """
+    Validates the connection to the chromadb server and returns the collection
+    """
+    try:
+        # docker run -d -p 8000:8000 chromadb/chromadb
+        client = chromadb.HttpClient(host=os.getenv('CHROMA_HOST'), port=8000)
+            
+        collection = client.get_or_create_collection(name="searchgpt",
+                                                     metadata={"hnsw:space": "cosine"})
+        return collection
+    except DatabaseError as e:
+        raise DatabaseError(f"Error in getting collection: {e}")
+    
+
+if __name__ == "__main__":
+    print(get_chromadb_collection())
